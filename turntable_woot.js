7dbd035135355a4e4f532ae15ba79e75695645fc
Paul <Juzkus@users.noreply.github.com>
2023-01-06 21:16:32 -0600

    Create turntable_woot.js

---
+++ turntable_woot.js
@@ -0,0 +1,1 @@
+javascript:(function(){  let wooted = {}; setInterval(function(){ if (wooted[song._id] == true) {return;} else{ document.querySelector('.awesome-button').click(); wooted[song._id] = true; }}, 1000);  })();
